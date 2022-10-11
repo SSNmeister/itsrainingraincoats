8e3b22818d9ca691901d06df190de084e891a9e7
SSNmeister <110984640+SSNmeister@users.noreply.github.com>
2022-10-11 22:04:41 +0800

    Initial commit. header, homepage, navbar. + steps modal

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import React, { useState } from "react";
+import ModalHowToApplyForItems from "./components/ModalHowToApplyForItems";
+import Header from "./Header";
+import NavBar from "./navbar/NavBar";
+import Home from "./pages/Home";
+
+function App() {
+  const [openModalApplyItems, setOpenModalApplyItems] = useState(false);
+
+  return (
+    <div className="main-app-container">
+      {!openModalApplyItems ? (
+        <>
+          <div className="main-header-container">
+            <Header />
+          </div>
+          <div className="main-body-container">
+            <Home setOpenModalApplyItems={setOpenModalApplyItems} />
+          </div>
+          <div className="main-navbar">
+            <NavBar />
+          </div>
+        </>
+      ) : (
+        <div className="main-modal-applyforitems-container">
+          <ModalHowToApplyForItems
+            setOpenModalApplyItems={setOpenModalApplyItems}
+          />
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default App;
